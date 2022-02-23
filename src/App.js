29f914d8f9d22a63b4d42eb6514f31948e7e74a5
ananthprasad.n1 <ananthprasad.n1@partner.samsung.com>
2022-02-23 23:01:18 +0530

    add colors and homepage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Table, Button } from "reactstrap";
 import { filteredData } from "./utils";
+import "./App.css";
+
 function App() {
   const [data, setData] = useState([]);
   const getData = () => {
@@ -33,7 +35,7 @@ function App() {
         </thead>
         <tbody>
           {data.map((dt) => (
-            <tr key={`tr${dt.symbol}`} color="primary">
+            <tr key={`tr${dt.symbol}`} color="primary" className={dt.status === "BUY" ? 'buy' : 'sell'}>
               <td scope="row" key={`symbol${dt.symbol}`}>
                 {dt.symbol}
               </td>
